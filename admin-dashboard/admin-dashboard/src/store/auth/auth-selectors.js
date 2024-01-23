3c97e40db1b544baaaa1dd38267d0a94b0f32fde
junaid-waqas <junaidwaqas509@gmail.com>
2024-01-23 20:06:22 +0500

    remove files

--- admin-dashboard/admin-dashboard/src/store/auth/auth-selectors.js
+++
@@ -1,7 +0,0 @@
-// selectors.js
-export const selectUser = (store) => ({
-    user: store.authSlice.auth.user,
-    token: store.authSlice.auth.token,
-    userType:store.authSlice.auth.userType,
-  });
-  
